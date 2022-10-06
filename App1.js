54a1b8856a2731f1d5a959d5f870b61fcf3b6dc3
9-ghb-6 <107903633+9-ghb-6@users.noreply.github.com>
2022-10-06 23:45:17 +0900

    2022-10-06

---
+++ App1.js
@@ -0,0 +1,27 @@
+import * as Notifications from 'expo-notifications';
+
+
+
+
+export default function App() {
+
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: false,
+            shouldSetBadge: false,
+        }),
+    });
+    return (
+        <></>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
